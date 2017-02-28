aaf65181ca57f977e8c56b2c31fd1b1de00941de
KarineMellata <karinemellata@wpa132152.wireless.mcgill.ca>
2017-02-28 18:43:44 -0500

    added persistence

--- ca/mcgill/ecse223/tileo/persistence/PersistenceObjectStream.java
+++ ca/mcgill/ecse223/tileo/persistence/PersistenceObjectStream.java
@@ -1,1 +1,38 @@
-to fill in
+package ca.mcgill.ecse223.tileo.persistence;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public static PersistenceObjectStream {
+
+    public static void serialize(String filename, Object object) {
+        FileOutputStream fileOut;
+        try {
+            fileOut = new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(object);
+            out.close();
+            fileOut.close();
+        } catch (Exception e) {
+            throw new RuntimeException("Could not save data to file'" + filename + ".");
+        }
+        }
+
+        public static Object deserialize(String filename){
+            Object o = null;
+            ObjectInputStream in;
+            try{
+                FileInputStream fileIn = new FileInputStream(filename);
+                in = new ObjectInputStream(fileIn);
+                o = in.readObject();
+                in.close();
+                fileIn.close();
+            }catch(Exception e){
+                o=null;
+            }
+        return o;
+        }
+
+        }
